05a8fd55f5b331b9e62e785fccef7d4c13c89c8b
Kai <katzkuma@gmail.com>
2023-08-09 14:48:27 +1000

    feat: Add MongoDB database integration
    - Integrated MongoDB as the database backend for the project.
    - Created a new database connection using Mongoose.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRouter = require('./routes/workouts')
 // initial app
 const app = express()
@@ -15,9 +16,16 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRouter)
 
-// start listening
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // start listening
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 
 module.exports = app
