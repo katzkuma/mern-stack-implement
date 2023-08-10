be6a6faa963a346c9edfd7a7b413ea7ec5e04706
Kai <katzkuma@gmail.com>
2023-08-10 12:01:41 +1000

    test: Add unit tests for get single workout API
    In this commit, unit tests for the "get single workout" API have been added. The tests include:
    - Calling the "get all workout" API to retrieve the id of the first workout and then testing if the API correctly returns the details of the specified workout.
    - Using an id (9999999999) that does not exist to test if the API properly returns an error message for non-existent ids.
    
    Changes Made:
    - Added unit tests for the "get single workout" API
    - Tested retrieving a workout using an existing id
    - Tested retrieving a workout using a non-existent id

--- backend/test/utils/expectStatus.js
+++ backend/test/utils/expectStatus.js
@@ -7,7 +7,12 @@ function expectStatusResponseToBe400(response) {
     expect(response.statusCode).toBe(400)
 }
 
+function expectStatusResponseToBe404(response) {
+    expect(response.statusCode).toBe(404)
+}
+
 module.exports = {
     expectStatusResponseToBe200,
-    expectStatusResponseToBe400
+    expectStatusResponseToBe400,
+    expectStatusResponseToBe404
 };
--- backend/test/workout.test.js
+++ backend/test/workout.test.js
@@ -1,28 +1,45 @@
 const server = require('../server')
 const request = require('supertest')(server)
 const { expectJsonResponse } = require('./utils/expectJson');
-const { expectStatusResponseToBe200, expectStatusResponseToBe400 } = require('./utils/expectStatus');
+const { 
+    expectStatusResponseToBe200, 
+    expectStatusResponseToBe400,
+    expectStatusResponseToBe404
+ } = require('./utils/expectStatus');
 
 describe('GET /workouts/:id', () => {
 
     describe('get a single workout', () => {
 
         test('should respond with a 200 status code ', async () => {
-            const response = await request.get("/api/workouts/1");
+            const firstWorkout = (await request.get("/api/workouts")).body[0]
+            const response = await request.get("/api/workouts/"+ firstWorkout._id);
+
             expectStatusResponseToBe200(response)
         });
         
         test('should specify json in the content type header ', async () => {
-            const response = await request.get("/api/workouts/1");
+            const firstWorkout = (await request.get("/api/workouts")).body[0];
+            const response = await request.get("/api/workouts/"+ firstWorkout._id);
+
             expectJsonResponse(response)
         });
 
         test('should get correct workouts data ', async () => {
-            const response = await request.get("/api/workouts/1");
+            const firstWorkout = (await request.get("/api/workouts")).body[0];
+            const response = await request.get("/api/workouts/"+ firstWorkout._id);
 
-            expect(response.body.mssg).toBeDefined()
+            expect(response.body).toBeDefined()
         });
         // should respond with a json object
+    });
+
+    describe('When id is not matched to any workout', () => {
+        test('should respond with a 400 status code ', async () => {
+            const response = await request.get("/api/workouts/9999999999");
+
+            expectStatusResponseToBe404(response)
+        });
     });
 });
 
@@ -41,7 +58,7 @@ describe('GET /workouts', () => {
         test('should get correct workouts data ', async () => {
             const response = await request.get("/api/workouts");
 
-            expect(response.body.mssg).toBeDefined()
+            expect(response.body).toBeDefined()
         });
     });
 });
