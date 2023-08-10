6db551fdc118ccd11f182ac227d1e401a462bb58
Kai <katzkuma@gmail.com>
2023-08-10 14:16:00 +1000

    feat: Add home component and Navbar for front-end homepage
    Changes Made:
    - Created a new home component to serve as the front-end homepage.
    - Added a Navbar component to provide navigation and improve user experience.
    - Implemented react-router-dom to enable routing and page navigation.

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link, link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <header>
+            <div className="container">
+                <Link to="/">
+                    <h1>Workout Buddy</h1>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
+
