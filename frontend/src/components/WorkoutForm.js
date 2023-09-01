7ebb1fdc26d6d838b764d92d9d75bb2141adf8f6
Kai <katzkuma@gmail.com>
2023-09-01 14:00:24 +1000

    feat: Enhance error responses handling on workout form
    To improve the user experience while creating a new workout data, web interface will highlight the incorrect input.
    
    Changes Made:
    1. add data validation and create a list for the incorrect input in createWorkout logic of workoutController.js
    2. display the incorrect input on WorkoutForm.js
    3. add relevant .css

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -5,7 +5,8 @@ const WorkoutForm = () => {
     const[title, setTitle] = useState('')
     const[load, setLoad] = useState('')
     const[reps, setReps] = useState('')
-    const[error, setError] = useState('')
+    const[error, setError] = useState(null)
+    const[emptyFields, setEmptyFields] = useState([])
 
     const { dispatch } = useWorkoutsContext()
 
@@ -26,6 +27,7 @@ const WorkoutForm = () => {
 
         if(!response.ok) {
             setError(json.error)
+            setEmptyFields(json.emptyFields)
         }
 
         if(response.ok) {
@@ -33,6 +35,7 @@ const WorkoutForm = () => {
             setLoad('')
             setReps('')
             setError(null)
+            setEmptyFields([])
             console.log('new workout added')
             dispatch({ type: "CREATE_WORKOUT", payload: json})
         }
@@ -46,18 +49,21 @@ const WorkoutForm = () => {
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes('title') ? 'error' : ''}
             />
             <label>Load (in kg):</label>
             <input 
                 type="number"
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
+                className={emptyFields.includes('load') ? 'error' : ''}
             />
             <label>Reps:</label>
             <input 
                 type="number"
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes('reps') ? 'error' : ''}
             />
 
             <button>Add Workout</button>
